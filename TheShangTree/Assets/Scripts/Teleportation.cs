d271204fd19a3dde81f6d8eb1113c5be4148b02c
mo768 <mg1301@live.mdc.ac.uk>
2021-11-18 22:48:55 +0000

    Teleportation complete
    Teleportation is now done. Finally.

---
+++ TheShangTree/Assets/Scripts/Teleportation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
