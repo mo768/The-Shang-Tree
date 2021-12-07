bf29bddf7ec56616766ee978247543abbd361815
mo768 <mg1301@live.mdc.ac.uk>
2021-12-07 18:06:28 +0000

    phone test
    the game works on phone but the controls dont seem to be fixed, the buttons are not in the right place but its all fine because it still works on android.

--- TheShangTree/Assets/Scripts/CharacterMovement.cs
+++ TheShangTree/Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -16,10 +17,10 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
-        var movement = Input.GetAxis("Horizontal");
+        var movement = CrossPlatformInputManager.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
